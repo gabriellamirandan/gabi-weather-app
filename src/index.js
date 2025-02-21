7ee83a11f8d02457083558d250531afdece23d52
gabriellamirandan <gabriella-miranda@hotmail.com>
2025-02-21 16:29:07 +0000

    Added condition to searchCity function and axios to html file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-function searchCity (event){
+function searchCity(event) {
     event.preventDefault();
     let cityInput = document.querySelector("#city-name")
     let cityElement = document.querySelector("#city-h1")
-    cityElement.innerHTML = cityInput.value;
+    if (cityInput.value) {
+        cityElement.innerHTML = cityInput.value;
+    } else {
+        alert('That is not a valid city name 🤔 Please try again.')
+    }
 }
 
 let searchFormElement = document.querySelector('#search-form');
