ca97e43fa7166523c85e29431dd701bc203c6fac
gabriellamirandan <gabriella-miranda@hotmail.com>
2025-02-21 17:13:22 +0000

    Injected more weather data with the API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
-function updateWeather (response) {
+function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature-value");
     let currentTemperature = response.data.temperature.current;
+
     let cityElement = document.querySelector("#city-h1");
+
+    let descriptionElement = document.querySelector("#weather-description");
+    let currentDescription = response.data.condition.description;
+
     let humidityElement = document.querySelector("#humidity");
     let currentHumidity = response.data.temperature.humidity;
+
     let windElement = document.querySelector("#wind");
     let currentWind = response.data.wind.speed;
 
+    let dateElement = document.querySelector("#date");
+    let currentDate = new Date();
+    
+    let timeElement = document.querySelector("#time");    
+
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(currentTemperature);
+    descriptionElement.innerHTML = currentDescription;
     humidityElement.innerHTML = currentHumidity;
-    windElement.innerHTML = currentWind
+    windElement.innerHTML = currentWind;
+    dateElement.innerHTML = formatDate(currentDate);
+    timeElement.innerHTML = formatTime(currentDate);
+
 }
 
-function searchCityAPI (city){
+function formatDate (date){
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    return `${month[date.getMonth()]} ${date.getDay()},`
+}
+
+function formatTime (date){
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10){
+        minutes = `0${minutes}`
+    }
+
+    if (hours < 10){
+        hours = `0${hours}`
+    }
+    return `${hours}:${minutes}`
+}
+
+function searchCityAPI(city) {
     let apiKey = "3e46b86b8308faao2ct16f0ddbe04ffa";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
     axios.get(apiUrl).then(updateWeather);
@@ -30,4 +63,6 @@ function searchCity(event) {
 }
 
 let searchFormElement = document.querySelector('#search-form');
-searchFormElement.addEventListener("submit", searchCity)
+searchFormElement.addEventListener("submit", searchCity);
+
+searchCityAPI("Lisbon");
